62cbadc92e0e19b0a48172ec1a0287efe5b340d1
YoussefMassoud <youssefbusiness133@gmail.com>
2024-07-04 05:40:03 +0300

    make product page

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,90 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function ProductsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className=" flex gap-x-2 items-center">
+          <Link href="/dashboard/products/create">
+            <PlusCircle className=" h-4 w-4 " />
+            <span>Add Product</span>
+          </Link>
+        </Button>
+      </div>
+      <div className="py-5">
+        <Card>
+          <CardHeader>
+            <CardTitle>Products</CardTitle>
+            <CardDescription>
+              Manage your products and view their sales perform
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Image</TableHead>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <UserIcon className="w-16 h-16" />
+                  </TableCell>
+                  <TableCell>Youssef Nike</TableCell>
+
+                  <TableCell className="text-green-600">$1000</TableCell>
+                  <TableCell>10/12/2024</TableCell>
+                  <TableCell className="text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size="icon" variant="ghost">
+                          <MoreHorizontal className="w-4 h-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Action</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
